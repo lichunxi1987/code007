ffc09b56037bef390b72ecb33190afcc823912aa
skylee <349018588@qq.com>
2019-07-08 21:10:24 +0800

    hello

---
+++ src/CalcAndAssign.java
@@ -0,0 +1,20 @@
+public class CalcAndAssign {
+
+    public static void main(String[] args) {
+
+        int a=8;
+
+        a >>=2;
+        System.out.println(a);
+
+        a*=9;
+        System.out.println(a);
+    }
+
+
+}
+/*这是一条华丽的分割线，以下内容都是注释
+
+
+
+ */
